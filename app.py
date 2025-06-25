60f6121506aabcb37b64c89cfc573fb4cf348f40
Johntherocker <cederdahl@gmail.com>
2025-06-25 23:15:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.docstore.document import Document
 from langchain.embeddings import OpenAIEmbeddings
 import google.generativeai as genai
